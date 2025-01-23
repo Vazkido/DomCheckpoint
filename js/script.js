f0ca3f16290e12f9d05f0b1cf64658809dad5716
valentine <vazkid02@gmail.com>
2025-01-23 16:49:36 -0500

    JavaScript update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,46 @@
-let products = document.querySelectorAll('.card-item')
+let products = document.querySelectorAll(".card-item");
 
-products.forEach ((item) => { 
+products.forEach((item) => {
+  let plus = item.querySelector(".fa-plus-circle");
+  let minus = item.querySelector(".fa-minus-circle");
+  let delBtn = item.querySelector(".fa-trash-alt");
+  let heart = item.querySelector(".fa-heart");
+  let qty = item.querySelector(".quantity");
+  let quantity = parseInt(qty.textContent);
+  let price = parseInt(
+    item.querySelector(".unit-price").textContent.replace("$", "")
+  );
+  plus.addEventListener("click", () => {
+    quantity++;
+    qty.innerHTML = quantity;
+    calcTotal("add");
+  });
+  minus.addEventListener("click", () => {
+    if (quantity > 0) {
+      quantity--;
+      qty.textContent = quantity;
+      calcTotal("subtract");
+    }
+  });
+  delBtn.addEventListener("click", () => {
+    let total = document.querySelector(".total");
+    total.textContent = parseInt(total.textContent) - price * quantity;
+    item.remove();
+  });
+  heart.addEventListener("click", () => {
+    if (heart.style.color === "red") {
+      heart.style.color = "black";
+    } else {
+      heart.style.color = "red";
+    }
+  });
 
-    let plus = item.querySelector('.fa-plus-circle');
-    let minus = item.querySelector('.fa-minus-circle');
-    let delBtn = item.querySelector('.fa-trash-alt');
-    let heart = item.querySelector('.fa-heart');
-    
-})
-
-
+  const calcTotal = (operation) => {
+    let total = document.querySelector(".total");
+    if (operation === "add") {
+      total.textContent = parseInt(total.textContent) + price;
+    } else {
+      total.textContent = parseInt(total.textrContent) - price;
+    }
+  };
+});
